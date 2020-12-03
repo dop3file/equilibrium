2d83dce2da0ceaad803a5fbff66d2ccb539aaa42
Renat Yakublevich <49581209+RenatYakublevich@users.noreply.github.com>
2020-12-03 22:12:00 +0300

    3.1.2
    hot fix

--- library/files.py
+++ library/files.py
@@ -1,3 +1,7 @@
+import os
+import exceptions
+
+
 class Files:
     def __init__(self):
         pass
@@ -14,6 +18,30 @@ class Files:
         self.name_file.close()
         with open(self.name_file_prev, 'r', encoding='utf-8') as file:
             return [line.strip() for line in file]
+        self.name_file = open(self.name_file_prev, 'a', encoding='utf-8')
+
+    def delete_file(self, value):
+        try:
+            self.name_file.close()
+        except AttributeError:
+            exceptions.File_Connection('Файл не подключён')
+        os.remove(value.replace("'",""))
+
+    def update_file(self, line, value_for_edit):
+        try:
+            self.name_file.close()
+        except AttributeError:
+            exceptions.File_Connection('Файл не подключён')
+        with open(self.name_file_prev,'r', encoding='utf-8') as file:
+            all_files = [line.strip() for line in file]
+            all_files[int(line)] = value_for_edit.replace("'","")
+
+        with open(self.name_file_prev,'w', encoding='utf-8') as file:
+            file.write('\n'.join(all_files))
+        self.name_file = open(self.name_file_prev,'a',encoding='utf-8')
+
+
+
 
 
 
