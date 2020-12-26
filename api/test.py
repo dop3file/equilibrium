d586009dabb516dfc5c5406287070c7a8cd45b9e
Renat Yakublevich <49581209+RenatYakublevich@users.noreply.github.com>
2020-12-27 02:12:17 +0300

    Update test.py

--- api/test.py
+++ api/test.py
@@ -6,7 +6,7 @@ import requests
 
 
 # URL, на который собираетесь отправлять запрос
-url = 'http://127.0.0.1:5000/api'
+url = 'http://equilibriumweb.pythonanywhere.com/api'
 
 # Параметры запроса
 params = {
